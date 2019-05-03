a648ba8c428623686933a89395320b5d88228ab0
ghozziayoub <ghozziayoub@gmail.com>
2019-05-03 23:05:26 +0100

    rectif sql

--- atb.sql
+++ atb.sql
@@ -1,7 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  sam. 04 mai 2019 à 00:04
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `pfe_atb`
+--
 
 -- --------------------------------------------------------
 
@@ -9,19 +28,24 @@ SET time_zone = "+00:00";
 -- Structure de la table `accounts`
 --
 
-DROP TABLE IF EXISTS `accounts`;
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `num_account` varchar(255) NOT NULL,
+CREATE TABLE `accounts` (
+  `num_account` int(11) NOT NULL,
   `name_account` varchar(255) NOT NULL,
   `type_account` varchar(255) NOT NULL,
   `RIB` varchar(255) NOT NULL,
   `solde` varchar(255) NOT NULL,
   `devise_account` varchar(255) NOT NULL,
-  `id_client` varchar(50) NOT NULL,
-  PRIMARY KEY (`num_account`),
-  KEY `id_client` (`id_client`)
+  `id_client` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `accounts`
+--
+
+INSERT INTO `accounts` (`num_account`, `name_account`, `type_account`, `RIB`, `solde`, `devise_account`, `id_client`) VALUES
+(1, 'Name', 'epargne', '7630001007941234567890185', '500', 'EUR', 1),
+(2, 'Name 2', 'epargne', '7630004000031234567890143', '1200', 'TND', 1),
+(3, 'name 3', 'epargne', '7630006000011234567890189', '200', 'USD', 1);
 
 -- --------------------------------------------------------
 
@@ -29,16 +53,21 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 -- Structure de la table `historique`
 --
 
-DROP TABLE IF EXISTS `historique`;
-CREATE TABLE IF NOT EXISTS `historique` (
-  `id` varchar(50) NOT NULL,
+CREATE TABLE `historique` (
+  `id` int(11) NOT NULL,
   `type_transaction` varchar(255) NOT NULL,
   `date_transaction` date NOT NULL,
   `montant` varchar(255) NOT NULL,
-  `num_compte` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `num_compte` (`num_compte`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `num_compte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `historique`
+--
+
+INSERT INTO `historique` (`id`, `type_transaction`, `date_transaction`, `montant`, `num_compte`) VALUES
+(1, 'retrait', '2019-05-01', '100', 1),
+(2, 'depot ', '2019-05-02', '50', 2);
 
 -- --------------------------------------------------------
 
@@ -46,18 +75,86 @@ CREATE TABLE IF NOT EXISTS `historique` (
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` varchar(50) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
-  `pass_account` varchar(50) NOT NULL,
+  `pass_account` varchar(100) NOT NULL,
   `tel` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `adresse` varchar(50) NOT NULL,
-  `encrypted_password` varchar(80) NOT NULL,
-  `salt` varchar(10) NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `adresse` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `nom`, `prenom`, `pass_account`, `tel`, `email`, `adresse`) VALUES
+(1, 'ayoub', 'Ghozzi', '81dc9bdb52d04dc20036dbd8313ed055', '25836914', 'ghozzi@gmail.com', '19 , Rue Yahia Turki , Lafayette , Tunis');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`num_account`),
+  ADD KEY `id_client` (`id_client`);
+
+--
+-- Index pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `num_compte` (`num_compte`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `num_account` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `historique`
+--
+ALTER TABLE `historique`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD CONSTRAINT `historique_ibfk_1` FOREIGN KEY (`num_compte`) REFERENCES `accounts` (`num_account`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
