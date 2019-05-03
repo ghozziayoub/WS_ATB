ace066b0bdca6858f627533eb1c9a24928830e03
ghozziayoub <ghozziayoub@gmail.com>
2019-05-03 23:56:18 +0100

    rectif reponse adding id login

--- login.php
+++ login.php
@@ -12,11 +12,13 @@ $passcrypt = md5($pass);
 $req = "SELECT * FROM users where email='$email' AND pass_account='$passcrypt'";
 
 $res = $bd->query($req);
+$row = $res->fetch(PDO::FETCH_ASSOC);
 
 $nb=$res->rowCount();
 
 if ($nb>0) {
     $result["reponse"]=true;
+    $result["id"]=$row["id"];
 }else {
     $result["reponse"]=false;
 }
